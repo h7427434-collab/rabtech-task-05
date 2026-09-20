d98aefc3d320cae76def00c7f9209bb269228431
Heena I <h7427434@gmail.com>
2026-09-20 22:26:31 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,11 @@
+const API_URL = "https://fakestoreapi.com/products";
+
+async function fetchProducts() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return await response.json();
+}
